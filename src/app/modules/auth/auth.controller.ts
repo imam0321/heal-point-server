84326aa808433b473a47e38da31ee59b5cadc316
imam0321 <imam.hossain0321@gmail.com>
2025-10-11 18:34:09 +0600

    feat: Add setAuthCookie utility and update credentialLogin to set authentication cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,9 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
 import { AuthService } from "./auth.service";
 import sendResponse from "../../utils/sendResponse";
+import { setAuthCookie } from "../../utils/setCookies";
 
 const credentialLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const result = await AuthService.credentialLogin(req.body);
+
+  setAuthCookie(res, result)
 
   sendResponse(res, {
     statusCode: 201,
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,5 +1,11 @@
+import { Gender } from "@prisma/client"
+
 export interface ICreatePatientInput {
   name: string
   email: string
   password: string
+  profilePhoto?: string
+  contactNumber?: string
+  address?: string
+  gender?: Gender
 }
